b8c14c3cbd88a5a4e2a13549e4c4526cda7f5056
Carina-P <carina.palmqvist@telia.com>
2020-07-06 07:53:36 +0000

    Js-script to collapse menu when moving in small screen

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,8 @@
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+/* This code is inspired from byllsa: https://github.com/byIlsa/Aloy-from-outcast-to-heroine */
+$('.js-action').on('click', function () {
+  $('.navbar-collapse').collapse('hide');
+});
