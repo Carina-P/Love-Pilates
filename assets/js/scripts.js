045f364626d94c402a88cafa30fc56572932c9e3
Carina-P <carina.palmqvist@telia.com>
2020-07-08 18:14:06 +0000

    Not using popover anymore so took away that code from scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,4 @@
-/* I got this from bootstrap */
-
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
-
+ 
 /* This code is inspired from byllsa: https://github.com/byIlsa/Aloy-from-outcast-to-heroine */
 $('.js-action').on('click', function () {
   $('.navbar-collapse').collapse('hide');
